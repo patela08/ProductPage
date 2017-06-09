1e540eaf4fc38220ebde70d0ef05d5045a5907bb
patela08 <patelakshar.patel98@gmail.com>
2017-06-09 13:07:02 -0400

    First commit

---
+++ app/app.component.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+// import { Hero } from './hero';
+// const HEROES: Hero[] = [
+//   { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' },
+// ];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.pageTitle = 'Product Management';
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n            <div>\n              <h1>{{pageTitle}}</h1>\n              <my-products></my-products>\n            </div>\n"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+// export class myApp { }
+//# sourceMappingURL=app.component.js.map
---
+++ app/products/product-list-component.ts
@@ -0,0 +1,10 @@
+    import { Component } from '@angular/core';
+
+    @Component({
+        selector: 'my-products' ,
+        templateUrl: 'app/products/product-list-component.html'
+    })
+
+    export class ProductListComponet{
+
+    }
